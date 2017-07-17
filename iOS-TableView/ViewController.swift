0c31eb0f7e04e1a861cc07f5627ae88e66e5d6ff
John Colley <webjocky@gmail.com>
2017-07-17 14:44:08 -0700

    initial commit

--- iOS-TableView/ViewController.swift
+++ iOS-TableView/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tasks = [String]()
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self as UITableViewDataSource
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        if tasks.contains(textField.text!) {
+            let idx = tasks.index(of: textField.text!)
+            let indexPath = IndexPath(row: idx!, section: 0)
+            tableView.selectRow(at: indexPath, animated: true, scrollPosition: .bottom)
+            tableView.delegate?.tableView!(tableView, didSelectRowAt: indexPath)
+            
+            textField.textColor = UIColor.red
+        } else {
+            tasks.append(textField.text!)
+            tableView.reloadData()
+            textField.text = ""
+        }
+    }
 
+    @IBAction func textFieldChanged(_ sender: UITextField) {
+        textField.textColor = UIColor.black
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+    
+}
